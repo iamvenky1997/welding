0c68e84a5787c4c6d5c6f124c07f15f2dc0dc17e
iamvenky1997 <iamvenky1997@gmail.com>
2023-02-21 22:08:46 +0530

    git pages

--- src/Component/Routing/routes.js
+++ src/Component/Routing/routes.js
@@ -2,6 +2,7 @@ import { Route,Routes } from "react-router-dom"
 import About from "../About/about"
 import Services from "../Services.js/services"
 import Home from "../Home/home"
+import NotFound from "../notfound/notFound"
 
 const Routing  = () =>{
 return (
@@ -9,8 +10,7 @@ return (
     <Route path="/" element = {<Home />} ></Route>
     <Route path="/about" element= {<About />}></Route>
     <Route path="/services" element= {<Services />}></Route>
-    
-  
+    <Route path="*" element= {<NotFound />}></Route>
 </Routes>
 )
 }
---
+++ src/Component/notfound/notFound.js
@@ -0,0 +1,8 @@
+const NotFound = () =>{
+    return(
+        <>
+        <img src="https://img.freepik.com/free-vector/flat-404-error-template_23-2147746977.jpg?w=740&t=st=1676899086~exp=1676899686~hmac=3b124ad3c71e108f1dc254e2a03f77c5932a2cc27d3554a1539d9cc7e6ac1f1c" />
+        </>
+    )
+}
+export default NotFound
